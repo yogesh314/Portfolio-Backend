93c1827b66d7070dd4ec9232f3177e2835223252
yogesh314 <yogeshatworld3.14@gmail.com>
2025-07-05 03:32:09 +0530

    8. Making s3 file open directly in browser instead of start downloading file.

--- Portfolio-Backend/src/main/java/com/yogesh/Portfolio_Backend/service/BaseService.java
+++ Portfolio-Backend/src/main/java/com/yogesh/Portfolio_Backend/service/BaseService.java
@@ -2,7 +2,10 @@ package com.yogesh.Portfolio_Backend.service;
 
 import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public abstract class BaseService {
@@ -16,8 +19,31 @@ public abstract class BaseService {
 
     protected String uploadFile(String fileName, byte[] fileData) {
         String key = UUID.randomUUID().toString() + "_" + fileName;
-        s3Client.putObject(r -> r.bucket(bucketName).key(key), software.amazon.awssdk.core.sync.RequestBody.fromBytes(fileData));
+
+        //Map file extensions to Content-Types
+        String contentType = getContentType(fileName);
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(contentType) // Set the Content-Type
+                .build();
+
+        s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromBytes(fileData));
         return s3Client.utilities().getUrl(b -> b.bucket(bucketName).key(key)).toString();
+    }
+
+    private static String getContentType(String fileName) {
+        Map<String, String> contentTypes = new HashMap<>();
+        contentTypes.put(".jpg", "image/jpeg");
+        contentTypes.put(".jpeg", "image/jpeg");
+        contentTypes.put(".png", "image/png");
+        contentTypes.put(".pdf", "application/pdf");
+
+        return contentTypes.getOrDefault(
+                fileName.substring(fileName.lastIndexOf(".")).toLowerCase(),
+                "application/octet-stream" // Default if extension not recognized
+        );
     }
 
     protected void deleteFile(String url) {
